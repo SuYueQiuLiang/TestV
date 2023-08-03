461a5afd400c0714fb26d4cf56d2c4552ee0b8c6
srtTB <qiuliangsuyue@163.com>
2023-08-03 14:03:10 +0800

    change gradle plugin and gradle version

--- repositories.gradle.kts
+++ repositories.gradle.kts
@@ -1,5 +1,5 @@
 rootProject.extra.apply {
-    set("androidPlugin", "com.android.tools.build:gradle:8.0.2")
+    set("androidPlugin", "com.android.tools.build:gradle:7.4.2")
     set("kotlinVersion", "1.8.20")
 }
 
